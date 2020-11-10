7ed5da41a26cc24a3f7d284486adf8699ac5021c
ph <ph@whu.edu.cn>
2020-11-10 23:17:43 +0800

    modify

--- PMI.py
+++ PMI.py
@@ -81,6 +81,9 @@ class PMI:
         log_per = math.log(per, 2)
         return log_per
 
+    def calculate_sopmi(self, p, n):
+        return p - n
+
     def get_pmi(self):
         """
         function:返回符合阈值的pmi列表
@@ -91,8 +94,11 @@ class PMI:
         negative = ('送得慢', '难吃', '糟糕', '太贵', '差劲','差评')
         dict_pmi = {}
         dict_frq_word = self.get_dict_frq_word()
-        print (dict_frq_word)
+        i = 1
+      #  print (dict_frq_word)
         for word1 in dict_frq_word:
+            positive_pmi = 0
+            negative_pmi = 0
             wordpercent1 = dict_frq_word[word1]
             for word2 in positive: 
                 # dict_frq_word:
@@ -102,11 +108,33 @@ class PMI:
                 list_together=[]
                 list_together.append(word1)
                 list_together.append(word2)
-                together_probability = self.calcularprobability(self.document, list_together)
-                if together_probability > self.minitogether:
-                    string = word1 + ' , ' + word2 + ', positive, PMI:'
-                    dict_pmi[string] = self.calculate_nmi(together_probability, wordpercent1, wordpercent2)
-        
+                together_probability_pos = self.calcularprobability(self.document, list_together)
+                if together_probability_pos > self.minitogether:
+                    positive_pmi = positive_pmi + self.calculate_nmi(together_probability_pos, wordpercent1, wordpercent2)
+                
+            for word3 in negative:
+                if word1 == word3:
+                    continue
+                wordpercent3 = dict_frq_word[word3]
+                list_together = []
+                list_together.append(word1)
+                list_together.append(word3)
+                together_probability_neg = self.calcularprobability(self.document, list_together)
+                if together_probability_neg > self.minitogether:
+                    negative_pmi = negative_pmi + self.calculate_nmi(together_probability_neg, wordpercent1, wordpercent3)
+          #  print('done')
+          #  if flag_pos != 0 and flag_neg != 0:
+              #  if together_probability_pos > self.minitogether and together_probability_neg > self.minitogether:
+            string = word1 + ' , '+ ' PMI:'
+            dict_pmi[string] = self.calculate_sopmi(positive_pmi, negative_pmi)
+            print('store the SOPMI: done', i)
+            i = i + 1
+            if i > 100:
+                break
+        print('all new words done, return the main')
+        return dict_pmi
+                #    dict_pmi[string] = self.calculate_nmi(together_probability, wordpercent1, wordpercent2)
+        """
         for word1 in dict_frq_word :
             wordpercent1 = dict_frq_word[word1]
             for word2 in negative:
@@ -121,3 +149,5 @@ class PMI:
                     string = word1 + ' , ' + word2 + ', negative, PMI:'
                     dict_pmi[string] = self.calculate_nmi(together_probability, wordpercent1, wordpercent2)
         return dict_pmi
+    
+"""
--- textPMI.py
+++ textPMI.py
@@ -16,11 +16,15 @@ if __name__ == '__main__':
             words = extract(sentences)
             for word in words:
                 extractwords.append(word)
-            documents.append(set(extractwords))
+                documents.append(set(extractwords))
     pm = PMI(documents)
     pmi = pm.get_pmi()
     print('get PMI already, start sorting...', '\n')
-    sorted(pmi.calculate.nmi)
+    sorted(pmi.values())
     for p in pmi:
-        if pmi[p] > 4:
-            print (p, pmi[p], '\n')
+        if pmi[p] > 0.0:
+            print (p, pmi[p], ' positive','\n')
+        if pmi[p] < 0 :
+            print (p, pmi[p], ' negative', '\n')
+        if pmi[p] == 0:
+            print (p, pmi[p], ' middle ', '\n')
